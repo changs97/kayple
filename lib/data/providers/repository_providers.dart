d9386debca7c5edd1bc667185af95ee9a40313b5
changs97 <kin2027@gmail.com>
2025-11-17 19:16:25 +0900

    iOS 네트워크 이슈 수정: - Cloudflare에 의해 API 요청이 차단(blocked) 이슈 - user-agent를 명시

--- lib/data/providers/repository_providers.dart
+++ lib/data/providers/repository_providers.dart
@@ -9,7 +9,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioProvider = Provider<Dio>((ref) {
-  final dio = Dio();
+  final dio = Dio(BaseOptions(
+    headers: {
+      'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1',
+      'Accept': 'application/json',
+      'Accept-Language': 'en-US,en;q=0.9',
+      'Referer': 'https://jsonplaceholder.typicode.com/',
+    },
+  ));
   dio.interceptors.add(NetworkLogInterceptor());
   return dio;
 });
